87651be1e4b36bcdb0d3551eea35aef9fd65bfa1
Delavalom <laarvelo@icloud.com>
2023-02-13 16:24:32 -0400

    dropbox upload file challenge

---
+++ dropbox-file-uploads/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <main className='w-full h-screen'>
+      <h1>hello</h1>
+    </main>
+  )
+}
+
+export default App
---
+++ dropbox-file-uploads/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,18 @@
+import { type FC } from 'react';
+
+type Props = {};
+
+export const Navbar: FC<Props> = ({}) => {
+
+  return (
+    <header>
+        <h1>Dropbox</h1>
+        <nav>
+            <input type="search" name="" id="" />
+        </nav>
+        <picture>
+            <img src="" alt="question mark" />
+        </picture>
+    </header>
+  );
+};
