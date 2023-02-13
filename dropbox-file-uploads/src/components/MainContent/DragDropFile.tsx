7706beb051eb17563a6c16ae4e1b5ced99c0a5b4
Delavalom <laarvelo@icloud.com>
2023-02-13 18:34:22 -0400

    implement drag and drop component

---
+++ dropbox-file-uploads/src/components/MainContent/DragDropFile.tsx
@@ -0,0 +1,80 @@
+import { useState, type FC, useRef, ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+
+export const DragDropFile: FC<Props> = ({ children }) => {
+  const [dragActive, setDragActive] = useState(false);
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleDrag = (e: any) => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (e.type === "dragenter" || e.type === "dragover") {
+      setDragActive(true);
+    } else if (e.type === "dragleave") {
+      setDragActive(false);
+    }
+  };
+  // triggers when file is dropped
+  const handleDrop = (e: any) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setDragActive(false);
+    if (e.dataTransfer?.files && e.dataTransfer?.files[0]) {
+      // handleFiles(e.dataTransfer.files);
+    }
+  };
+  // triggers when file is selected with click
+  const handleChange = (e: any) => {
+    e.preventDefault();
+    if (e.target.files && e.target.files[0]) {
+      // handleFiles(e.target.files);
+    }
+  };
+
+  const onButtonClick = () => {
+    inputRef.current?.click();
+  };
+
+  return (
+    <form
+      id="input-file-upload"
+      onSubmit={(e) => e.preventDefault()}
+      onDragEnter={handleDrag}
+      className={`w-full h-full flex p-4 flex-col gap-2 ${
+        dragActive ? "border-4 border-dashed border-slate-800" : ""
+      }`}
+    >
+      <input
+        id="input-file-upload"
+        ref={inputRef}
+        placeholder="Upload Files"
+        type="file"
+        className="hidden"
+        multiple={true}
+        onChange={handleChange}
+      />
+      <button
+        onClick={onButtonClick}
+        className="bg-slate-800 text-slate-50 hover:text-slate-900 hover:bg-slate-200 transition-colors duration-300"
+      >
+        Upload File
+      </button>
+      <label htmlFor="input-file-upload">
+        {dragActive && (
+          <section
+            id="drag-file-element"
+            onDragEnter={handleDrag}
+            onDragLeave={handleDrag}
+            onDragOver={handleDrag}
+            onDrop={handleDrop}
+            className="w-full h-full absolute top-0 right-0 bottom-0 left-0"
+          ></section>
+        )}
+        {children}
+      </label>
+    </form>
+  );
+};
--- dropbox-file-uploads/src/components/MainContent/MainContent.tsx
+++ dropbox-file-uploads/src/components/MainContent/MainContent.tsx
@@ -1,78 +1,15 @@
-import {
-  ChangeEvent,
-  DragEvent,
-  useState,
-  type FC,
-  ChangeEventHandler,
-  DragEventHandler,
-  useRef,
-} from "react";
+import { type FC } from "react";
 import { Table } from "../Table/Table";
+import { DragDropFile } from "./DragDropFile";
 
 type Props = {};
 
 export const MainContent: FC<Props> = ({}) => {
-  const [dragActive, setDragActive] = useState(false);
-  const inputRef = useRef<HTMLInputElement | null>(null);
-
-  const handleDrag = (e: DragEvent) => {
-    e.preventDefault();
-    e.stopPropagation();
-    if (e.type === "dragenter" || e.type === "dragover") {
-      setDragActive(true);
-    } else if (e.type === "dragleave") {
-      setDragActive(false);
-    }
-  };
-  // triggers when file is dropped
-  const handleDrop = (e: DragEvent) => {
-    e.preventDefault();
-    e.stopPropagation();
-    setDragActive(false);
-    if (e.dataTransfer.files && e.dataTransfer.files[0]) {
-      // handleFiles(e.dataTransfer.files);
-    }
-  };
-  // triggers when file is selected with click
-  const handleChange = (e: any) => {
-    e.preventDefault();
-    if (e.target.files && e.target.files[0]) {
-      // handleFiles(e.target.files);
-    }
-  };
   return (
-    <form
-      id="input-file-upload"
-      onSubmit={(e) => e.preventDefault()}
-      onDragEnter={handleDrag}
-      className="w-full h-full flex p-4 flex-col gap-2"
-    >
-      <input
-        id="input-file-upload"
-        ref={inputRef}
-        placeholder="Upload Files"
-        type="file"
-        className="hidden"
-        multiple={true}
-        onChange={handleChange}
-      />
-      <button className="">Upload File</button>
-      <label htmlFor="input-file-upload">
-        {dragActive && (
-          <section
-            id="drag-file-element"
-            onDragEnter={handleDrag}
-            onDragLeave={handleDrag}
-            onDragOver={handleDrag}
-            onDrop={handleDrop}
-            className={`w-full h-full ${
-              dragActive ? "border-dashed border-slate-800" : ""
-            }`}
-          >
-            <Table />
-          </section>
-        )}
-      </label>
-    </form>
+    <DragDropFile>
+      <section className="w-full h-full">
+        <Table />
+      </section>
+    </DragDropFile>
   );
 };
